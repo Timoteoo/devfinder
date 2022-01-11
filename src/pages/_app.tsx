c46d14080d2fb29efcf855e3dd65e5ef89206990
Timoteo <randomthecoder1557@gmail.com>
2022-01-11 16:29:18 -0300

    🌞 feat: wrap entire app with searchcontext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SearchContextProvider } from "../contexts/SearchContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SearchContextProvider>
+      <Component {...pageProps} />
+    </SearchContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
