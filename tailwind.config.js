ca4fdbd29ebf9dfb59a0d35a2e61fb9b2103d74b
Timoteo <randomthecoder1557@gmail.com>
2022-01-11 16:25:54 -0300

    🌞 feature: add new colors to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './src/pages/**/*.tsx',
     './src/components/**/*.tsx'
   ],
   theme: {
+    colors: {
+      ...colors,
+      'blue-gray': '#1f2b49',
+      'light-blue': '#0476fa'
+    },
     extend: {},
   },
   plugins: [],
