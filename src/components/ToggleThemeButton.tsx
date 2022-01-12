a15af79ff5e07fbe51f09a08cf561190b58dd484
Timoteo <randomthecoder1557@gmail.com>
2022-01-12 10:58:55 -0300

    🌞 feat: implement animations on toggletheme button
    Also fixed a problem where the button text and icon did remain the same no matter which theme the app had.
    Now, if its dark, then the button says "LIGHT" and shows a Sun icon.
    Otherwise, it says "DARK" and shows a Moon icon.

--- src/components/ToggleThemeButton.tsx
+++ src/components/ToggleThemeButton.tsx
@@ -1,5 +1,22 @@
 import { useThemeSwitch } from "../hooks/useThemeSwitch";
+import { AnimatePresence, motion, Variants } from "framer-motion";
 import SunIcon from "./icons/SunIcon";
+import MoonIcon from "./icons/MoonIcon";
+
+const buttonVariants: Variants = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+  },
+  exit: {
+    x: 100,
+    opacity: 0,
+  },
+};
 
 function ToggleThemeButton() {
   const { setTheme, theme } = useThemeSwitch();
@@ -9,10 +26,37 @@ function ToggleThemeButton() {
   }
 
   return (
-    <button className="flex items-center" onClick={handleClick}>
-      <p className="mr-4 text-sm tracking-widest font-bold">LIGHT</p>
-      <SunIcon />
-    </button>
+    <div className="flex items-center">
+      <AnimatePresence exitBeforeEnter initial={false}>
+        {theme === "light" ? (
+          <motion.button
+            key={1}
+            className="flex items-center"
+            onClick={handleClick}
+            variants={buttonVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            <p className="mr-4 text-sm tracking-widest font-bold text-slate-900">DARK</p>
+            <MoonIcon />
+          </motion.button>
+        ) : (
+          <motion.button
+            key={2}
+            className="flex items-center"
+            onClick={handleClick}
+            variants={buttonVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            <p className="mr-4 text-sm tracking-widest font-bold text-white">LIGHT</p>
+            <SunIcon />
+          </motion.button>
+        )}
+      </AnimatePresence>
+    </div>
   );
 }
 
