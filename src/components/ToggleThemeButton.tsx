3bf20bb22ab1dcc1c7f0b050036aaf938902a5cd
Timoteo <randomthecoder1557@gmail.com>
2022-01-12 10:09:37 -0300

    🌞 feat: implement theme switch on themebutton click

--- src/components/ToggleThemeButton.tsx
+++ src/components/ToggleThemeButton.tsx
@@ -1,8 +1,15 @@
+import { useThemeSwitch } from "../hooks/useThemeSwitch";
 import SunIcon from "./icons/SunIcon";
 
 function ToggleThemeButton() {
+  const { setTheme, theme } = useThemeSwitch();
+
+  function handleClick() {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  }
+
   return (
-    <button className="flex items-center">
+    <button className="flex items-center" onClick={handleClick}>
       <p className="mr-4 text-sm tracking-widest font-bold">LIGHT</p>
       <SunIcon />
     </button>
